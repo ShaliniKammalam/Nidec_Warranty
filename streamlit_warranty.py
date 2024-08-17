1f985409f72a853992fbbff360a782d77aca2805
Shalini Kammalam <115170181+ShaliniKammalam@users.noreply.github.com>
2024-08-17 21:17:16 +0530

    Update streamlit_warranty.py

--- streamlit_warranty.py
+++ streamlit_warranty.py
@@ -1212,7 +1212,7 @@ def main():
 
         # Extract all available Fiscal Years from the dataset
         fiscal_years = extract_fiscal_years(data)
-        st.sidebar.image('NidecPower.jpeg', width=230)
+        st.sidebar.image('NidecPower.jpg', width=230)
 
         # Sidebar table selection
         selected_table = st.sidebar.selectbox("Select Table", ["Cumulative Data", "Complaints", 
